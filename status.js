8475d7e203af9107cba5ad65346e87973d9b8f01
yakimaguro <maguroshouta@gmail.com>
2022-04-28 10:37:02 +0900

    status-message

--- status.js
+++ status.js
@@ -2,6 +2,7 @@ var time = 500;
 $('span').each(function(i, element) {
     setTimeout(function() {
         $(element).delay(1000 * i).css('background-color', "#278773")
+        $('#status-message').delay(1000 * i).text('Stage ' + (i + 1) + '/18')
     }, time)
-    time += Math.floor(Math.random() * (20000 + 1 - 2000)) + 2000;
+    time += Math.floor(Math.random() * (20000 + 1 - 5000)) + 5000;
 })
